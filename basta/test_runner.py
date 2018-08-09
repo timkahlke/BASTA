9f2e7dd971faa2968a20d4d14e596968bd4d5a4c
timkahlke <tim.kahlke@audiotax.is>
2018-08-09 12:36:24 +1000

    ... packaging ...

--- basta/test_runner.py
+++ basta/test_runner.py
@@ -1,20 +1,19 @@
 import unittest
 
-import db_utils
-import tax_creator
-import tax_tree
-import file_utils
-import download_utils
+from basta import db_utils
+from basta import tax_creator
+from basta import tax_tree
+from basta import file_utils
+from bastaimport download_utils
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
-suite.addTests(loader.loadTestsFromModule(db_utils))
-suite.addTests(loader.loadTestsFromModule(download_utils))
-suite.addTests(loader.loadTestsFromModule(file_utils))
-suite.addTests(loader.loadTestsFromModule(tax_creator))
-suite.addTests(loader.loadTestsFromModule(tax_tree))
+suite.addTests(loader.loadTestsFromModule(test_db_utils))
+suite.addTests(loader.loadTestsFromModule(test_download_utils))
+suite.addTests(loader.loadTestsFromModule(test_file_utils))
+suite.addTests(loader.loadTestsFromModule(test_tax_creator))
+suite.addTests(loader.loadTestsFromModule(test_tax_tree))
 
 runner = unittest.TextTestRunner(verbosity=3)
 result = runner.run(suite)
-
