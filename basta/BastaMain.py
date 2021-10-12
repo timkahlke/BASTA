b4b6cabedef89ed53595b41f231ccdc0939b0366
timkahlke <tim.kahlke@audiotax.is>
2021-10-13 08:58:13 +1100

    correct download bug, adjust py3 calls xrange, plyvel etc

--- basta/BastaMain.py
+++ basta/BastaMain.py
@@ -92,7 +92,7 @@ class Main():
     def _basta_sequence(self,args):
         self.logger.info("\n#### Assigning taxonomy to each sequence ###\n")
         db_file = dbutils.get_db_name(args.directory,args.type)
-        assigner = AssignTaxonomy.Assigner(args.evalue,args.alen,args.identity,args.number,args.minimum,args.maj_perc,args.directory,args.config_path,args.output,args.hit_count)
+        assigner = AssignTaxonomy.Assigner(args.evalue,args.alen,args.identity,args.number,args.minimum,args.maj_perc,args.directory,args.config_path,args.output,args.hit_count,args.quiet)
         if args.verbose:
             assigner.info_file = args.verbose
         assigner._assign_sequence(args.blast,db_file,args.best_hit)
@@ -102,7 +102,7 @@ class Main():
     def _basta_single(self,args):
         self.logger.info("\n#### Assigning one taxonomy based on all sequences ###\n")
         db_file = dbutils.get_db_name(args.directory,args.type)
-        assigner = AssignTaxonomy.Assigner(args.evalue,args.alen,args.identity,args.number,args.minimum,args.maj_perc,args.directory,args.config_path,args.output,args.hit_count) 
+        assigner = AssignTaxonomy.Assigner(args.evalue,args.alen,args.identity,args.number,args.minimum,args.maj_perc,args.directory,args.config_path,args.output,args.hit_count,args.quiet) 
         if args.verbose:
             assigner.info_file = args.verbose
         lca = assigner._assign_single(args.blast,db_file,args.best_hit)
@@ -115,7 +115,7 @@ class Main():
         self.logger.info("\n####  Assigning one taxonomy for each file ###\n")
         db_file = ""
         db_file = dbutils.get_db_name(args.directory,args.type)
-        assigner = AssignTaxonomy.Assigner(args.evalue,args.alen,args.identity,args.number,args.minimum,args.maj_perc,args.directory,args.config_path,args.output,args.hit_count)
+        assigner = AssignTaxonomy.Assigner(args.evalue,args.alen,args.identity,args.number,args.minimum,args.maj_perc,args.directory,args.config_path,args.output,args.hit_count,args.quiet)
         if args.verbose:
             assigner.info_file = args.verbose
         assigner._assign_multiple(args.blast,db_file,args.best_hit)
